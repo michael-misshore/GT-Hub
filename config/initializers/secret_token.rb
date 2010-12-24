9cd5d22bff60c53a25ccd76df20f5b9ee7e6874d
Michael Misshore <mmisshore@gmail.com>
2010-12-24 10:57:21 -0600

    Need to ignore the config directory. It will be deployed vbia Capistrano

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-GTHub::Application.config.secret_token = '0c9ad123226ba6447effd01c3debb4d4085198b440c2ae894303719401d91a16e803a8e3a6988f8bb729cb0b20469ea8d70fe0e9188af000ebbc5e9cb1fc2545'
