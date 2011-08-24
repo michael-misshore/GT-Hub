1b0bef1082ca7ad161efe62e9bd6af0f681fe6ba
Michael Misshore <mmisshore@gmail.com>
2011-08-23 20:23:38 -0500

    Cleaned up Car import file and initial importer.

--- app/models/car.rb
+++ app/models/car.rb
@@ -23,9 +23,9 @@ class Car < ActiveRecord::Base
       elsif !block.strip.empty?
         car = {:make => @make}
         # Get the year of the vehicle
-        year = block.match(/\'[0-9]{2}\s/);
+        year = block.match(/\'[0-9]{2}/);
         if !year.nil?
-          block.gsub!(year[0], "") # We no longer need the year in the line
+          block = block.gsub(year[0], "") # We no longer need the year in the line
           year = year.to_s.gsub(/\'/, '').to_i
           # I am decided to store the year in a 4 digit format
           # There maybe a better way to do this, but this works... for now. :)
@@ -35,13 +35,13 @@ class Car < ActiveRecord::Base
         
         # Get the type of car (Standard or Premium) : Assuming we have a type for all lines
         type = block.match(/(\[st\])|(\[pr\])/)
-        block.gsub!(/(\[st\])|(\[pr\])/, '') # We no longer need the type(s) in the line
+        block = block.gsub(/(\[st\])|(\[pr\])/, '') # We no longer need the type(s) in the line
         car[:premium] = type.to_s.include?('[pr]') ? true : false
         car[:standard] = type.to_s.include?('[st]') ? true : false
           
         # remove make and the beginning asterik so we have just the model left
         block.gsub!(/#{@make}|\*/, '')
-        car[:model] = block
+        car[:model] = block.gsub(/(\[\[)|(\]\])/, '').gsub(/(\s)+/, ' ')
         
         car = Car.new(car)
         car.save
