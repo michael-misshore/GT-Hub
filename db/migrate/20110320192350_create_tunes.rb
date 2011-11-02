18eef00eb53d519ccaa2ed5974d6efca1659cfb5
Michael Misshore <mmisshore@gmail.com>
2011-11-01 21:57:19 -0500

    Name/Class Mismatch

--- db/migrate/20110320192350_create_tunes.rb
+++ db/migrate/20110320192350_create_tunes.rb
@@ -2,7 +2,7 @@ class CreateTunes < ActiveRecord::Migration
   def self.up
     create_table :tunes do |t|
       t.integer :car_id
-      t.integer :user_id
+      t.integer :tuner_id
       t.integer :track_id
       t.integer :karma, :null => false, :default => 0
       t.string :tune_type, :null => false, :default => 'Grip'
